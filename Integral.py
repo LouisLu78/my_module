96f7102ce7382ab711f7a92a6721acf88ae2c885
Louislu78 <lewgq@yahoo.com>
2019-11-23 16:00:21 +0800

    I believe I have mastered the basic programming skills of this language and I need more practice. As a well-trained chemist with more than 20 years of expertise, I try to apply python in  the field of science.

---
+++ Integral.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# author: Guangqiang Lu time:2019/11/23
+
+class Integral:
+    def __init__(self, f, a, n=100):
+        self.f, self.a, self.n = f, a, n
+
+    def trapezoidal(self, f, a, x, n):
+        self.f,self.a,self.x,self.n=f,a,x,n
+        h = (x - a) / n
+        I = 0.5 * f(a)
+        for i in range(1, n):
+            I += f(a + i * h)
+        I += 0.5 * f(x)
+        I *= h
+        return I
+
+    def __call__(self, x):
+        return self.trapezoidal(self.f, self.a, x, self.n)
+
+def _verify():
+
+    def f(x):
+        return 2*x**2
+    G=Integral(f,0,n=100)
+    result=G(10)
+    print(result)
+
+if __name__=='__main__':
+    _verify()
