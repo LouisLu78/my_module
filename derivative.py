f63ec52a3e649d0d9a7cdb3482577832ca90dc60
Louislu78 <lewgq@yahoo.com>
2019-11-23 21:29:54 +0800

    I believe I have mastered the basic programming skills of this language and I need more practice. As a well-trained chemist with more than 20 years of expertise, I try to apply python in  the field of science.

--- derivative.py
+++ derivative.py
@@ -7,7 +7,7 @@ def derivative(f, x, h=1E-5):
 
 def _verify():
     from math import cos, pi
-    print(derivative(cos, pi/6))
+    print('The derivative of cos(30) is %.4f.'%derivative(cos, pi/6))
 
 if __name__=='__main__':
     _verify()
