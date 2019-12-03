c3cf0b80183c0f0625999d58dbafd3a37dc4edb2
Louislu78 <lewgq@yahoo.com>
2019-12-03 21:56:01 +0800

    I believe I have mastered the basic programming skills of this language and I need more practice. As a well-trained chemist with more than 20 years of expertise, I try to apply python in  the field of science.

--- vectors_angle.py
+++ vectors_angle.py
@@ -4,20 +4,23 @@
 
 def angle(x, y):
     import math
-    list(x),list(y)
-    if len(x)==2:
-        x.append(0), y.append(0)
-    dot=abs(sum(x[i]*y[i] for i in range(3)))
-    abs_x=math.sqrt(sum(x[i]**2 for i in range(3)))
-    abs_y = math.sqrt(sum(y[i] ** 2 for i in range(3)))
+    x_list=list(x)
+    y_list=list(y)
+    if len(x_list)==2:
+        x_list.append(0), y_list.append(0)
+    dot=sum(x_list[i]*y_list[i] for i in range(3))
+    abs_x=math.sqrt(sum(x_list[i]**2 for i in range(3)))
+    abs_y = math.sqrt(sum(y_list[i]**2 for i in range(3)))
     cos_value=dot/(abs_x*abs_y)
     angle=math.acos(cos_value)
     return angle*180/math.pi
 
 
 def _verify():
-    x=(-1,0,1)
-    y=(1,0,1)
+    # x=(-1,0,2)
+    # y=(2,0,1)
+    x=(5,-7)
+    y=(-6,-4)
     print(angle(x,y))
 
 if __name__=='__main__':
