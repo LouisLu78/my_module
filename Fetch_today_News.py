23ddf9b9b3cc3a1b8d2bd27099f835182cb01185
Louislu78 <lewgq@yahoo.com>
2019-12-10 21:39:35 +0800

    I believe I have mastered the basic programming skills of this language and I need more practice. As a well-trained chemist with more than 20 years of expertise, I try to apply python in  the field of science.

--- Fetch_today_News.py
+++ Fetch_today_News.py
@@ -17,16 +17,18 @@ urls={'Yahoo':'http://news.yahoo.com','MSN':'http://www.msn.com/en-us/news/world
 
 for website in urls:
     res=requests.get(urls[website], stream=True)
-    if res.status_code ==200:
+    if res.status_code != 200:
+        print('The internet connection for {} doesn\'t work today.'.format(website))
+        continue
+
+    else:
         print('The website for {} is connected.'.format(website))
         with open('news.html', 'wb') as fnews:
             for chunk in res.iter_content(100000):
                 fnews.write(chunk)
-        filename=os.path.join(newsfolder, '{}_news_{}.html'.format(website, Today))
+        filename = os.path.join(newsfolder, '{}_news_{}.html'.format(website, Today))
         shutil.move('news.html', filename)
         print('done')
-    else:
-        print('The internet connection for {} doesn\'t work today.'.format(website))
 
 print('Enjoy yourself in reading today\'s news.')
 
