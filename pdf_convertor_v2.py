c996458b4ae74cf54fcc49ca7f3348d36ad47bd7
Louislu78 <lewgq@yahoo.com>
2020-03-16 21:47:09 +0800

    My Python 2020

--- pdf_convertor_v2.py
+++ pdf_convertor_v2.py
@@ -11,23 +11,23 @@ class Convertor:
     counter=0
 
     def __init__(self):
-        Convertor.counter+=1
-        self.path_wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-        self.config=pdfkit.configuration(wkhtmltopdf=self.path_wkhtmltopdf)
+        Convertor.counter += 1
+        self.path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+        self.config = pdfkit.configuration(wkhtmltopdf=self.path_wkhtmltopdf)
         if not os.path.exists('.\\temp'):
             os.mkdir('.\\temp')
 
-        self.pdf_file='D:\\Program files\\JetBrains\\my_module\\temp\\tmp{}.pdf'.format(Convertor.counter)
+        self.pdf_file = 'D:\\Program files\\JetBrains\\my_module\\temp\\tmp{}.pdf'.format(Convertor.counter)
 
     def url_to_pdf(self, url):
-        pdfkit.from_url(url, self.pdf_file, configuration=self.config)
+        pdfkit.from_url(url, self.pdf_file, configuration = self.config)
         return self.pdf_file
 
     def html_to_pdf(self, html):
-        pdfkit.from_file(html, self.pdf_file, configuration=self.config)
+        pdfkit.from_file(html, self.pdf_file, configuration = self.config)
         return self.pdf_file
 
     def string_to_pdf(self, string):
-        pdfkit.from_string(string, self.pdf_file, configuration=self.config)
+        pdfkit.from_string(string, self.pdf_file, configuration = self.config)
         return self.pdf_file
 
